5cb96b0a3cfaf8e98a84603ac3d35af9bd673a17
Quentin Billac <quentin.billac2@gmail.com>
2022-09-16 15:02:50 +0200

    ajout react helmet page home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet } from "react-helmet-async";
 
 export const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Helmet | Home </title>
+      </Helmet>
         <h1>Page Home</h1>
     </div>
   )
